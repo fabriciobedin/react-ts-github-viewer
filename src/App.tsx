23c09b0560ba6c319b93f6d41d5a745c961af50f
Fabricio Bedin <fabricioobedin@gmail.com>
2020-08-08 22:12:45 -0300

    adding vscode config, font and started working with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-const App = () => {
-  return <h1>Hello World</h1>;
-};
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+    <GlobalStyle />
+  </BrowserRouter>
+);
 
 export default App;
